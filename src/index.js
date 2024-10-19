4776b1314b81f9db4498d4c5078b53b9390016bb
Александра Щетинина <aleksandrasetinina8@gmail.com>
2024-10-19 19:42:59 +0300

    feat: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let s = [...bracketsConfig];
+  let stack = [];
+  let brackets = {
+  ")": "(",
+  "}": "{",
+  "]": "[",
+  "|": "|"
+  };
+
+  for (let i = 0; i < s.length; i++){
+    const cur = s[i];
+
+    if(isClosedBracket(cur)){
+      if (brackets[cur] !== stack.pop()) return false;
+
+    }else {
+      stack.push(cur);
+    }
+  }
+
+  return stack.length === 0;
+
+}
+
+function isClosedBracket (item){
+  return[')','}',']','|'].indexOf(item) > -1;
 }
